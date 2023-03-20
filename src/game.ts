e1168b9350f5bd33d2f715816de86cdc6295a378
Juan Alcalde <juan@gambitstudio.com>
2023-03-20 21:40:02 +0900

    wip

--- src/game.ts
+++ src/game.ts
@@ -1,49 +1,37 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { log } from "@graphprotocol/graph-ts";
 import {
-  Game,
   FoundersPackPurchased,
-  ApprovalForAll,
   NftLocked,
-  NftUnlocked,
   TransferBatch,
   TransferSingle,
-  URI,
-  WithdrawalConfirmed
-} from "../generated/Game/Game"
-import { ExampleEntity } from "../generated/schema"
+  WithdrawalConfirmed,
+} from "../generated/Game/Game";
 
 export function handleFoundersPackPurchased(
   event: FoundersPackPurchased
 ): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
+  // let entity = ExampleEntity.load(event.transaction.from.toHex());
   // Entities only exist after they have been saved to the store;
   // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
+  // if (!entity) {
+  //   entity = new ExampleEntity(event.transaction.from.toHex());
+  // Entity fields can be set using simple assignments
+  // entity.count = BigInt.fromI32(0);
+  // }
   // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
+  // entity.count = entity.count + BigInt.fromI32(1);
   // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.characterNumber = event.params.characterNumber
-
+  // entity.owner = event.params.owner;
+  // entity.characterNumber = event.params.characterNumber;
   // Entities can be written to the store with `.save()`
-  entity.save()
-
+  // entity.save();
   // Note: If a handler doesn't require existing field values, it is faster
   // _not_ to load the entity from the store. Instead, create it fresh with
   // `new Entity(...)`, set the fields that should be updated and save the
   // entity back to the store. Fields that were not set or unset remain
   // unchanged, allowing for partial updates to be applied.
-
   // It is also possible to access smart contracts from mappings. For
   // example, the contract that has emitted the event can be connected to
   // with:
@@ -114,16 +102,12 @@ export function handleFoundersPackPurchased(
   // - contract.uri(...)
 }
 
-export function handleApprovalForAll(event: ApprovalForAll): void {}
-
 export function handleNftLocked(event: NftLocked): void {}
-
-export function handleNftUnlocked(event: NftUnlocked): void {}
 
 export function handleTransferBatch(event: TransferBatch): void {}
 
-export function handleTransferSingle(event: TransferSingle): void {}
-
-export function handleURI(event: URI): void {}
+export function handleTransferSingle(event: TransferSingle): void {
+  log.info("TRANSFER SINGLEEE", []);
+}
 
 export function handleWithdrawalConfirmed(event: WithdrawalConfirmed): void {}
